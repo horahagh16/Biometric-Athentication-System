1c5011566edba85bb5de861d7bb6377f6533fe9e
horahagh16 <haghighatkhah.hora@gmail.com>
2024-07-02 14:48:41 +0330

    add descriptor

--- fingerprint.py
+++ fingerprint.py
@@ -8,7 +8,7 @@ def get_descriptors(img):
     img = clahe.apply(img)
     img = np.array(img, dtype=np.uint8)
     # Threshold
-    _ , img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
     # Normalize to 0 and 1 range
     img[img == 255] = 1
 
@@ -25,18 +25,26 @@ def get_descriptors(img):
         for y in range(0, harris_normalized.shape[1]):
             if harris_normalized[x][y] > threshold_harris:
                 keypoints.append(cv2.KeyPoint(y, x, 1))
+    # Define descriptor
+    orb = cv2.ORB_create()
+    # Compute descriptors
+    _, des = orb.compute(img, keypoints)
 
-    return keypoints
+    # Convert descriptors to a string
+    if des is not None:
+        descriptors_str = ' '.join(map(str, des.flatten()))
+    else:
+        descriptors_str = ''
 
-# Load image
+    return descriptors_str
+
 img1 = cv2.imread('102_1.tif', cv2.IMREAD_GRAYSCALE)
-kp1, des1 = get_descriptors(img1)
+des1 = get_descriptors(img1)
 
 # Concatenate keypoints into a single string
-keypoints_str = ' '.join([f"({kp.pt[0]}, {kp.pt[1]})" for kp in kp1])
+keypoints_str = ' '.join([f"({des.pt[0]}, {des.pt[1]})" for des in des1])
 
 # Compute SHA-256 hash of the concatenated keypoints string
 sha256_hash = hashlib.sha256(keypoints_str.encode()).hexdigest()
 
 print(sha256_hash)
-#af1a289a075718672b3cc5a86bda57d211a07583daf66bf6b0851de4aad32b89
